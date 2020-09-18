39aa05e81a7db3f45519d05587bc9ac572967e6a
Ramesh493-sys <you@example.com>
2020-09-18 05:45:09 +0530

    Add set and get methods

--- CscExample.java
+++ CscExample.java
@@ -2,4 +2,42 @@ public class CscExample.java{
         int questionNo;
         String category;
         String text;
+
+
+	public void setQuestionNo(int questionNo)
+        {
+
+
+                this.questionNo = questionNo;
+
+        }
+
+        public void setCategory(String category)
+        {
+                this.category = cayegory;
+        }
+
+        public void setText(String text)
+        {
+                this.text = text;
+
+        }
+
+        public int getQuestioNo()
+        {
+                return questionNo;
+        }
+
+        public String getCategory()
+        {
+                return category;
+        }
+
+        public String gettext()
+        {
+                return text;
+        }
+
+
+
 }
